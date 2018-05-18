ac185aab5329fedaca3c87b39cf1622d41dddb6a
Anshul <ashivha@rccl.com>
2018-05-18 14:30:12 +0530

    Add changes for login service

--- knol-plm/login-impl/src/main/java/com/knoldus/Constants/QueryConstants.java
+++ knol-plm/login-impl/src/main/java/com/knoldus/Constants/QueryConstants.java
@@ -3,5 +3,5 @@ package com.knoldus.Constants;
 public class QueryConstants {
     
     public static final String LOGIN_TYPE_BY_USERNAME = "SELECT * FROM knolway"
-            + ".login_table WHERE username = ?";
+            + ".login_table WHERE email_id = ?";
 }
