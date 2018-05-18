2570dd59b3f8fe131c835fecdd872bb7dfee6ff3
neelaksh <nilaxch1@gmail.com>
2018-05-18 21:50:38 +0530

    Added CORS header to login service

--- knol-plm/login-api/src/main/java/com/knoldus/LoginService.java
+++ knol-plm/login-api/src/main/java/com/knoldus/LoginService.java
@@ -5,12 +5,15 @@ import com.knoldus.logininfo.LoginInfo;
 import com.knoldus.logininfo.UserInfo;
 import com.lightbend.lagom.javadsl.api.Descriptor;
 import com.lightbend.lagom.javadsl.api.Service;
+import com.lightbend.lagom.javadsl.api.ServiceAcl;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
+import com.lightbend.lagom.javadsl.api.transport.Method;
+
 import static com.lightbend.lagom.javadsl.api.Service.named;
 import static com.lightbend.lagom.javadsl.api.Service.pathCall;
 
 
-public interface LoginService extends Service{
+public interface LoginService extends Service {
     
     ServiceCall<LoginInfo, UserInfo> login();
     
@@ -18,8 +21,9 @@ public interface LoginService extends Service{
     @Override
     default Descriptor descriptor() {
         return named("login").withCalls(
-                pathCall("/api/login", this::login)
-        ).withAutoAcl(true);
+                pathCall("/api/login", this::login))
+                .withAutoAcl(true)
+                .withServiceAcls(ServiceAcl.methodAndPath(Method.OPTIONS, "/api/.*"));
         // @formatter:on
     }
 }
---
+++ knol-plm/login-impl/src/main/java/com/knoldus/MyCORSFilter.java
@@ -0,0 +1,15 @@
+package com.knoldus;
+
+import play.filters.cors.CORSFilter;
+import play.http.DefaultHttpFilters;
+
+import javax.inject.Inject;
+
+// See https://playframework.com/documentation/2.5.x/CorsFilter
+public class MyCORSFilter extends DefaultHttpFilters {
+    @Inject
+    public MyCORSFilter(CORSFilter corsFilter) {
+        super(corsFilter);
+    }
+}
+
